a2c3814c55c1cbc12416156c5df2157477fc274d
Renne Rocha <me@rennerocha.com>
2011-10-19 12:02:28 -0200

    incluindo novos campos no cadastro de um associado

--- lhc/associados/models.py
+++ lhc/associados/models.py
@@ -1,11 +1,15 @@
 #-*- coding: utf-8 -*-
 from django.db import models
 
-# Create your models here.
 class Associado(models.Model):
     nome = models.CharField(max_length=100)
+    apelido = models.CharField(max_length=100)
     email = models.EmailField()
     telefone = models.CharField(max_length=12)
 
+    ativo = models.BooleanField(default=True)
+
     def __unicode__(self):
-        return self.nome
+        if self.apelido and self.nome:
+            return '%s (%s)' % (self.nome, self.apelido)
+        return self.nome or self.apelido
