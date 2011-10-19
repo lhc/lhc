d3cd89fc7c180bd5d75d35c3e852bd505dd4db6d
Renne Rocha <me@rennerocha.com>
2011-10-19 21:19:30 -0200

    refatoração

--- lhc/associados/admin.py
+++ lhc/associados/admin.py
@@ -2,4 +2,8 @@
 from django.contrib import admin
 from associados.models import Associado
 
-admin.site.register(Associado)
+class AssociadoAdmin(admin.ModelAdmin):
+  list_filter = ['em_atraso', 'ativo']
+  list_display = ['nome', 'valor_mensalidade', 'ativo', 'em_atraso']
+
+admin.site.register(Associado, AssociadoAdmin)
--- lhc/pagamentos/admin.py
+++ lhc/pagamentos/admin.py
@@ -5,3 +5,18 @@ from pagamentos.models import Pagamento, Doacao, Despesa
 admin.site.register(Pagamento)
 admin.site.register(Despesa)
 admin.site.register(Doacao)
+
+class DoacaoAdmin(admin.ModelAdmin):
+  list_filter = ['data', 'origem', 'tipo']
+  list_display = ['origem', 'data', 'valor', 'membro', 'tipo']
+  fieldsets = [
+    (None,		{'fields': ['valor', 'data', 'membro', 'tipo']}),
+    ('Origem', 		{'fields': ['origem', 'referencia']}),
+  ]
+
+class DespesaAdmin(admin.ModelAdmin):
+  list_filter = ['data', 'tipo']
+  list_display = ['data', 'tipo', 'descricao', 'responsavel', 'valor']
+
+admin.site.register(Doacao, DoacaoAdmin)
+admin.site.register(Despesa, DespesaAdmin)
