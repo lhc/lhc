c4aa611da186c5c2f71c604e326bf6ea61aefa40
Renne Rocha <me@rennerocha.com>
2011-10-29 17:41:27 +0100

    removendo obrigatoriedade de algumas informações no cadastro de associados

--- associados/models.py
+++ associados/models.py
@@ -3,11 +3,11 @@ from django.db import models
 
 class Associado(models.Model):
     nome = models.CharField(max_length=100)
-    apelido = models.CharField(max_length=100, null=True)
+    apelido = models.CharField(max_length=100, blank=True, null=True)
     email = models.EmailField()
-    telefone = models.CharField(max_length=12, null=True)
+    telefone = models.CharField(max_length=12, blank=True, null=True)
 
-    valor_mensalidade = models.DecimalField(decimal_places=4, max_digits=10)
+    valor_mensalidade = models.DecimalField(decimal_places=2, max_digits=10, blank=True, null=True)
     ativo = models.BooleanField(default=True)
 
     def __unicode__(self):
