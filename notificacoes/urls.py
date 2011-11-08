17a5ec721aa64b0b3b43d5613785ef5902962dbd
Renne Rocha <me@rennerocha.com>
2011-11-08 15:51:47 +0100

    alterando urls de notificação para que elas não sejam reencaminhadas (301)

--- notificacoes/urls.py
+++ notificacoes/urls.py
@@ -3,8 +3,8 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
-    url(r'^pagseguro/', 'notificacoes.views.processa_pagseguro', name='notificacao-pagseguro'),
+    url(r'^pagseguro', 'notificacoes.views.processa_pagseguro', name='notificacao-pagseguro'),
 
-    url(r'^moip/', 'notificacoes.views.processa_moip', name='notificacao-moip'),
+    url(r'^moip', 'notificacoes.views.processa_moip', name='notificacao-moip'),
 )
 
