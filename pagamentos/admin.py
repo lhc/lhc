e5a3883071722caa003649000307e648d6c9aca3
Renne Rocha <me@rennerocha.com>
2011-11-09 13:48:58 +0100

    organizando melhor a apresentação dos lançamentos na interface do admin

--- pagamentos/admin.py
+++ pagamentos/admin.py
@@ -3,11 +3,11 @@ from django.contrib import admin
 from pagamentos.models import Lancamento
 
 class LancamentoAdmin(admin.ModelAdmin):
-  list_filter = ['data', 'origem', 'tipo']
-  list_display = ['origem', 'data', 'valor', 'associado', 'tipo']
-  fieldsets = [
-    (None,		{'fields': ['valor', 'data', 'associado', 'tipo']}),
-    ('Origem', 		{'fields': ['origem', 'referencia']}),
-  ]
+    list_filter = ['data', 'origem', 'tipo', 'associado', 'finalidade']
+    list_display = ['data', 'origem', 'descricao', 'valor', 'tipo', 'finalidade', 'associado']
+    fieldsets = [
+      ('Informações', {'fields': ['data', 'valor', 'descricao', 'associado', 'tipo', 'finalidade']}),
+      ('Origem', 		{'fields': ['origem', 'referencia',]}),
+    ]
 
 admin.site.register(Lancamento, LancamentoAdmin)
